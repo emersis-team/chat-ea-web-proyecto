ed2efaa2f5d1ce9cbbd017ae9a3f831543ef8c09
NahuelDeBellis <debellisnahuel@gmail.com>
2022-08-18 07:56:09 -0300

    add url for production server and add log in script

--- build_java.sh
+++ build_java.sh
@@ -3,8 +3,8 @@
 npm run build
 cp -r dist/ ~/cideso/chatea-api-front/src/main/resources/
 cd ~/cideso/chatea-api-front/src/main/resources/
-echo "moving to middleware..."
-pwd
+
+echo "Moving to middleware..."
 mv dist/index.html templates/index.html
 rm -r static/
 mv dist/ static/
@@ -12,3 +12,4 @@ mv dist/ static/
 echo "Building meaven package..."
 cd ~/cideso/chatea-api-front/
 mvn clean package
+echo "WAR builded. Ready to deploy. Bye" 
