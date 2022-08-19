0e41ed0a03f1d30dfc8de9d8ff0b25686ff3917e
NahuelDeBellis <debellisnahuel@gmail.com>
2022-08-19 12:30:52 -0300

    use some mediasoup methods, but idk when add a new video user/multiuser

--- src/peer/PeerConnection.ts
+++ src/peer/PeerConnection.ts
@@ -1,40 +1,58 @@
 import { CallHelper } from "../helpers/CallHelper";
 import { WebRtcConnection, EventsWebRtc } from "../types/WebRtcConnection";
-import { EnvSignaling } from "../types/Enviroment";
+import { io } from "socket.io-client";
 import mediaSoup from "mediasoup-client";
 
+let device: mediaSoup.Device;
+const room = io("", { room: "" });
+
+async function init() {
+
+	room.on(EventsWebRtc.open, (clientId) => { 
+		const RTPCapabilities = await room.emit("GetRtpCapabilities"); //room.emit()
+		device = new mediaSoup.Device();
+		device.load(RTPCapabilities);
+	});
+}
+
+/**
+	* Un `Producer` es el objeto para emitir video/audio a otros
+*/
+async function join() {
+	const trandportProducerData = await room.emit("createTransportProducer", {
+		rtpcapabilities: device.rtpCapabilites
+	});
+
+	const transportProducer = device.createSendTransport();
+	const localVideoStream = await CallHelper.loadLocalVideo();
+	const videoTrack = localVideoStream.getVideoTrack()[0];
+
+	await transportProducer.produce({ videoTrack });
+
+	initPeers();
+}
+
+async function initPeers() {
+	const transportConsumerData = room.emit("createTransportConsumer");
+	const transportConsumer = device.createRecvTransport(transportConsumerData);
+	transportConsumer.on("connect");
+	transportConsumer.on("connectionState");
+}
+     
+
 export class PeerConnection implements WebRtcConnection {
+	room: Object;
   usernameFrom: string;
   localVideo: MediaStream;
-  peer: Peer;
-  room: Room;
-  leave: boolean;
 
   constructor(from: string, localVideo: MediaStream) {
-		mediaSoup.Room();
-
     this.usernameFrom = from;
     this.leave = false;
 		this.localVideo = localVideo;
   }
 
   async connect() {
-    //this.peer = new Peer(this.usernameFrom);
-    let room: Room = new Room(this);
-
-		this.peer = new Peer(this.usernameFrom, {
-      host: EnvSignaling.PROD_HOST.valueOf(),
-			port: EnvSignaling.PROD_PORT.valueOf(),
-			path: '/satac',
-			secure: true // en local esta linea se comenta
-		});
-
-    this.peer.on(EventsWebRtc.open, (clientId) => { 
-			console.log("open");
-			room.joinRoom(clientId)
-		});
-     
-
+		let device;
     this.peer.on(EventsWebRtc.error, (e: Error) => {
       console.error("Intentando llamar", e.name, e.message);
       throw new Error("");
@@ -55,7 +73,7 @@ export class PeerConnection implements WebRtcConnection {
 			}
 		});
 
-    this.room = room;
+		this.device = device;
   }
 
   async toggleStatusCamAndMic(audio: boolean, video: boolean) {
